206db70969fa5ac730fa096abf406001cf2f855a
Daniil Tevs <daniil_tevs@mail.ru>
2024-06-21 13:56:54 +0200

    add logic for cache date

--- app/pages/options/options.controller.js
+++ app/pages/options/options.controller.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler'
 import { prisma } from '../../prisma.js'
+import { cache } from '../../utils/cache.utils.js'
 
 // @desc Get options by page
 // @route GET /api/page/:page/
@@ -9,24 +10,35 @@ export const getOptionsByPage = asyncHandler(async (req, res) => {
 	const { page } = req.params
 
 	try {
-		const options = {}
-		const optionsSource = await prisma.optionspage.findMany({
-			orderBy: {
-				sort: 'asc'
-			},
-			where: {
-				activity: true,
-				pages: { id: page }
-			},
-			select: {
-				name: true,
-				value: true
+		const data = cache.get(`pages/${page}.json`)
+		if (data) {
+			console.log('cache')
+			res.status(200).json(data)
+		} else {
+			const options = {}
+			const optionsSource = await prisma.optionspage.findMany({
+				orderBy: {
+					sort: 'asc'
+				},
+				where: {
+					activity: true,
+					pages: { id: page }
+				},
+				select: {
+					name: true,
+					value: true
+				}
+			})
+			optionsSource.forEach(option => {
+				options[option.name] = option.value
+			})
+			console.log('not cache')
+
+			if (Object.keys(options).length > 0) {
+				cache.set(`pages/${page}.json`, 216000, options)
 			}
-		})
-		optionsSource.forEach(option => {
-			options[option.name] = option.value
-		})
-		res.status(200).json(options)
+			res.status(200).json(options)
+		}
 	} catch (error) {
 		res.status(404)
 		throw new Error(error)
