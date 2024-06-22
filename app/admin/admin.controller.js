f13c05d7580ce64615007138afe8e9030c282c5f
Daniil Tevs <daniil_tevs@mail.ru>
2024-06-22 14:54:24 +0200

    create add, change and delete element table

--- app/admin/admin.controller.js
+++ app/admin/admin.controller.js
@@ -1,23 +1,14 @@
 import asyncHandler from 'express-async-handler'
 import { prisma } from '../prisma.js'
+import { getStructureTable } from '../utils/table.utils.js'
 
 // @desc Get data from table
 // @route GET /api/admin/:table/
-// @access Public
+// @access Private
 
 export const getDataTable = asyncHandler(async (req, res) => {
 	try {
 		const { table } = req.params
-
-		if (table === 'admin') {
-			res.status(401)
-			throw new Error('Данная таблица запрещена к изменению')
-		}
-
-		if (!prisma[table]) {
-			res.status(404)
-			throw new Error('Данная таблица не найдена')
-		}
 
 		const data = await prisma[table].findMany({
 			orderBy: {
@@ -26,6 +17,85 @@ export const getDataTable = asyncHandler(async (req, res) => {
 		})
 
 		res.status(200).json(data ? data : [])
+	} catch (error) {
+		res.status(404)
+		throw new Error(error)
+	}
+})
+
+// @desc Add element to table
+// @route GET /api/admin/:table/
+// @access Private
+
+export const addElement = asyncHandler(async (req, res) => {
+	try {
+		const { table } = req.params
+		console.log(table)
+
+		const element = await prisma[table].create({ data: req.body })
+
+		if (!element) {
+			res.status(400)
+			throw new Error(`Элемент не был добавлен`)
+		}
+
+		res.status(200).json(element ? element : false)
+	} catch (error) {
+		res.status(404)
+		throw new Error(error)
+	}
+})
+
+// @desc Update element in table
+// @route PUT /api/admin/:table/:id/
+// @access Private
+
+export const updateElement = asyncHandler(async (req, res) => {
+	try {
+		let { id, table } = req.params
+
+		const fields = getStructureTable(table)
+
+		if (fields.id.type === 'Int') id = +id
+
+		Object.keys(fields).forEach(field => {
+			if (field != 'id' && fields[field].type === 'Int' && req.body[field])
+				req.body[field] = +req.body[field]
+		})
+
+		if (fields.id.type === 'Int' && req.body.id) delete req.body.id
+
+		const element = await prisma[table].update({
+			data: req.body ? req.body : {},
+			where: {
+				id: id
+			}
+		})
+
+		res.status(200).json(element ? element : false)
+	} catch (error) {
+		res.status(404)
+		throw new Error(error)
+	}
+})
+
+// @desc Remove element from table
+// @route GET /api/admin/:table/:id/
+// @access Private
+
+export const removeElement = asyncHandler(async (req, res) => {
+	try {
+		const { id, table } = req.params
+
+		const fields = getStructureTable(table)
+
+		if (fields.id.type === 'Int') id = +id
+
+		const element = await prisma[table].delete({
+			where: { id }
+		})
+
+		res.status(200).json(element ? element : false)
 	} catch (error) {
 		res.status(404)
 		throw new Error(error)
--- app/admin/admin.routes.js
+++ app/admin/admin.routes.js
@@ -2,12 +2,31 @@ import Router from 'express'
 
 import authRoute from './auth/auth.routes.js'
 
-import { getDataTable } from './admin.controller.js'
+import { auth } from '../middleware/auth.middleware.js'
+import {
+	availableTable,
+	existTable,
+	validateParams
+} from '../middleware/table.middleware.js'
+import {
+	addElement,
+	getDataTable,
+	removeElement,
+	updateElement
+} from './admin.controller.js'
 
 const router = Router()
 
 router.use('/auth', authRoute)
 
-router.route('/:table').get(getDataTable)
+router
+	.route('/:table')
+	.get(auth, existTable, availableTable, getDataTable)
+	.post(auth, existTable, availableTable, validateParams, addElement)
+
+router
+	.route('/:table/:id')
+	.put(auth, existTable, availableTable, updateElement)
+	.delete(auth, existTable, availableTable, removeElement)
 
 export default router
